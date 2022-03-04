85145c9c205e2584e77169a7e5e7aea451589d9b
Wallesu <wallysonsouza4@hotmail.com>
2022-03-04 15:30:07 -0300

    first commit

---
+++ api/database/index.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const config = require("config");
+
+const instance = new Sequelize(
+  config.get("mysql.database"),
+  config.get("mysql.user"),
+  config.get("mysql.password"),
+  {
+    host: config.get("mysql.host"),
+    dialect: config.get("mysql.dialect"),
+  }
+);
